17b8bda9c69caf51ddfe8d264c594cb3b7204f19
Sergio Chaves Garcia-Mascaraque <sergiozteskate@gmail.com>
2020-12-03 14:05:20 +0000

    The quark mass is not defined as mq / L anymore.

--- include/Defs.hpp
+++ include/Defs.hpp
@@ -12,23 +12,24 @@ struct Defs // Structure containing all parameters in the class
     using CINTREF = const int&;
     using CDOBREF = const double&;
 
-    Defs(CINTREF Nt, CINTREF Ns, CINTREF Nc, CDOBREF xi, CDOBREF rs) :
-        Nt(Nt), Ns(Ns), Nc(Nc), xi(xi), rs(rs) {};
+    Defs(CINTREF Nt, CINTREF Ns, CINTREF Nc, 
+         CDOBREF xi, CDOBREF rs, CDOBREF mq) :
+        Nt(Nt), Ns(Ns), Nc(Nc), xi(xi), rs(rs), mq(mq) {};
+
     Defs(const Defs&) = default;
     ~Defs() = default;
 
     // -- Methods of the class {{{
     void Calculate_dk() { dk = (2 * PI) / Ns; }
     void Calculate_S0() { S0 = (4.0 * Nc) / (Ns * Ns * Ns); }
-    void Calculate_mq(const double& mqNs) { mq = mqNs / Ns; }
 
     // Setters of the class
-    void Set_Nt(CINTREF Nt)   { this->Nt = Nt; }
-    void Set_Ns(CINTREF Ns)   { this->Ns = Ns; }
-    void Set_Nc(CINTREF Nc)   { this->Nc = Nc; }
-    void Set_mq(CDOBREF mqNs) { this->mq = mqNs / Ns; }
-    void Set_xi(CDOBREF xi)   { this->xi = xi; }
-    void Set_rs(CDOBREF rs)   { this->rs = rs; }
+    void Set_Nt(CINTREF Nt) { this->Nt = Nt; }
+    void Set_Ns(CINTREF Ns) { this->Ns = Ns; }
+    void Set_Nc(CINTREF Nc) { this->Nc = Nc; }
+    void Set_mq(CDOBREF mq) { this->mq = mq; }
+    void Set_xi(CDOBREF xi) { this->xi = xi; }
+    void Set_rs(CDOBREF rs) { this->rs = rs; }
 
     void Set_Pext(CINTREF nPx, CINTREF nPy, CINTREF nPz) { 
         Pext[0] = nPx / (2 * PI * Ns); 
